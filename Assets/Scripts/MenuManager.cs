26bf3004059339584893a6fd15270b6eda9f7340
reandlyarahdian <52603915+reandlyarahdian@users.noreply.github.com>
2022-03-22 17:27:40 +0700

    Update Menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,13 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
-    public void Mode(int i)
+    public GameObject Single, Team, GameA, GameB, Mode;
+    public Button AI, Seek, Hide, TeamA, TeamB, Back, SingleBTN, TeamBTN, Base2an, Boy2an;
+
+    public void OpenSingle()
+    {
+        Single.SetActive(true);
+
+        Mode.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(AI.gameObject);
+    }
+
+    public void OpenTeam()
+    {
+        Team.SetActive(true);
+
+        Mode.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(TeamA.gameObject);
+    }
+
+    public void OpenMode()
+    {
+        Single.SetActive(false);
+        Team.SetActive(false);
+        Mode.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(SingleBTN.gameObject);
+    }
+
+    public void OpenGameA()
+    {
+        Single.SetActive(false);
+        GameA.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(Base2an.gameObject);
+    }
+    public void OpenGameB()
+    {
+        Single.SetActive(false);
+        GameB.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+
+        EventSystem.current.SetSelectedGameObject(Boy2an.gameObject);
+    }
+
+
+    public void ModeA(int i)
     {
         Passer.Instance.Mode(i);
-        SceneManager.LoadScene(1);
+    }
+
+    public void ModeB(int i)
+    {
+        Passer.Instance.Team(i);
+    }
+
+    public void SceneGame(int i)
+    {
+        SceneManager.LoadScene(i);
         Time.timeScale = 1f;
     }
 
